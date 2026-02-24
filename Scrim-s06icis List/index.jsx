26604d2e65f283264d7541f746dd3fbd02bc23ae
Ash-the-k <ashlesh.kanchan@gmail.com>
2026-02-25 01:58:26 +0530

    feat: Implementing React fundamentals for static pages
    Summary of changes:
    	- first-react : Initialized basic React setup.
    	- First React Code : Implemented initial React boilerplate and rendered first JSX elements.
    	- List : Created a basic unordered list utilizing React.
    	- Exercise 1 - Composable : Completed exercise demonstrating React's composable architecture.
    	- Exercise 1 - Declarative : Completed exercise showcasing React's declarative UI approach.

---
+++ Scrim-s06icis List/index.jsx
@@ -0,0 +1,16 @@
+/**
+ * Challenge: Set up a React app from scratch again.
+ * This time, try rendering an unordered list with 2-3 list items inside
+ * with why you're excited to be learning React.
+ */
+import { createRoot } from "react-dom/client"
+
+const root = createRoot(document.getElementById("root"))
+
+root.render(
+        <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+        </ul>
+    )
