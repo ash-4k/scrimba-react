2c27fb39ac3b7d36691fe21dd44e2e72f9c8ce65
Ash-the-k <ashlesh.kanchan@gmail.com>
2026-03-20 12:19:20 +0530

    feat: Initialize Tenzies capstone project and base setup
    Summary of changes:
    	- Project Introduction : Reviewed the core Tenzies game mechanics and project requirements (Folder 01).
    	- Initial Setup : Configured the base React application structure and initialized the starting boilerplate for the project (Folder 02).

---
+++ 5 Capstone Project 1 - Tenzies/02 Tenzies - Setup/p1s Scrim-s06j01j/index.jsx
@@ -0,0 +1,14 @@
+/**
+ * Challenge: Start a brand new React app!
+ * - Create a separate App component
+ * - Import and render the App component here
+ * - In the App component, render a <main> element
+ * - Style everything to look like the slide
+ */
+import ReactDom from 'react-dom/client'
+import App from './App'
+
+ReactDom
+    .createRoot(document.getElementById("root"))
+    .render(<App />)
+    
